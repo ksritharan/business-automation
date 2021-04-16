f3d79ba5bd180c22b13cf28cf23cc9c7174d9f55
ksritharan <kavin.sritharan@mail.utoronto.ca>
2021-04-16 13:58:29 -0400

    Update scanner.js

--- static/js/scanner.js
+++ static/js/scanner.js
@@ -6,9 +6,9 @@ $(document).keypress( function() {
   if (event.which == 13) {
     //tracking num can be in the format of (0000 0000 0000, AA 000 000 000 AA, 0000 0000 0000 0000)
     //if below filtering isn't good then probably need regex check or redirects
-    if (data.length == 12 || data.length == 13 || data.length == 16 || data.length == 27) {
-      if (data.length == 27) {
-        data = data.slice(7, 23); //2M1B3R587654321123456780000 -> 8765432112345678
+    if (data.length == 12 || data.length == 13 || data.length == 16 || data.length == 28) {
+      if (data.length == 28) {
+        data = data.slice(7, 23); //2M1B3R5876543211234567800000 -> 8765432112345678
       }
       location.assign("/orderinfo/"+data);
     }
